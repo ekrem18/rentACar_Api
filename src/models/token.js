9a0ef908f69aab19a2088fc7aa48eac8ced10ded
Ekrem <avekremyilmazturk@gmail.com>
2023-10-27 13:35:25 +0300

    uupp

--- src/models/token.js
+++ src/models/token.js
@@ -1,8 +1,6 @@
-"use strict"
-/* -------------------------------------------------------
-    NODEJS EXPRESS | CLARUSWAY FullStack Team
-------------------------------------------------------- */
-const { mongoose } = require('../configs/dbConnection')
+"use strict";
+/* -------------------------------------------------------*/
+const { mongoose } = require("../configs/dbConnection");
 /* ------------------------------------------------------- *
 {
   "userId": "65343222b67e9681f937f001",
@@ -10,3 +8,21 @@ const { mongoose } = require('../configs/dbConnection')
 }
 /* ------------------------------------------------------- */
 // Token Model:
+
+const Token = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    token: {
+      type: String,
+      trim: true,
+      required: true,
+      index: true                 //---> Sorgularda index'lediğimiz kısımlar hızlanıyor daha hızlı ulaşılıyor. Çok verilirse RAM şişer
+    },
+  },
+  {}
+);
