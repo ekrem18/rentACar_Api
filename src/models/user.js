44619f8826aa439f74aacb483ee655e8ef9b7574
Ekrem <avekremyilmazturk@gmail.com>
2023-10-27 12:25:17 +0300

    uupp

--- src/models/user.js
+++ src/models/user.js
@@ -75,13 +75,11 @@ const passwordEncrypt = required("../helpers/passwordEncrypt");
 
 UserSchema.pre(['save', 'updateOne'], function(next) {    //---> Kaydetmeden hemen önceki değişklikleri burada düzenliyorum. pre-save kullandığımız zaman arrow func yazmıyoruz
 
-  // get data from "this" when create;
-  // if process is updateOne, data will receive in "this._update"
   const data = this?._update || this
 
   // const emailRegExp = new RegExp("^\\w+([\\.-]?\\w+)*@\\w+([\\.-]?\\w+)*(\\.\\w{2,3})+$")
   // const isEmailValidated = emailRegExp.test(data.email)
-  // const isEmailValidated = RegExp("^\\w+([\\.-]?\\w+)*@\\w+([\\.-]?\\w+)*(\\.\\w{2,3})+$").test(data.email)
+  // const isEmailValidated = RegExp("^\\w+([\\.-]?\\w+)*@\\w+([\\.-]?\\w+)*(\\.\\w{2,3})+$").test(data.email)  //--->regex yapıldığında yazımı zorlaşıyor. iptal ediyorum
   const isEmailValidated = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(data.email) // test from "data".True False döndürür.
 
   if (isEmailValidated) {
