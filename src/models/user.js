d7c00728543b9569c41300b4bb5c9249b629390f
Ekrem <avekremyilmazturk@gmail.com>
2023-10-27 12:23:13 +0300

    edits

--- src/models/user.js
+++ src/models/user.js
@@ -67,31 +67,43 @@ const UserSchema = new mongoose.Schema(
     },
   },
   { collection: "users", timestamps: true }
-);                                                       //---> createdAt ve updatedAt timestamps sayesinde mongoose'ca yönetiliyor.
+);                                                         //---> createdAt ve updatedAt timestamps sayesinde mongoose'ca yönetiliyor.
 
 /* ------------------------------------------------------- */
 // Schema Configs
 const passwordEncrypt = required("../helpers/passwordEncrypt");
 
-UserSchema.pre("save", function (next) {
-                                                        //---> Kaydetmeden hemen önceki değişklikleri burada düzenliyorum. pre-save kullandığımız zaman arrow func yazmıyoruz
+UserSchema.pre(['save', 'updateOne'], function(next) {    //---> Kaydetmeden hemen önceki değişklikleri burada düzenliyorum. pre-save kullandığımız zaman arrow func yazmıyoruz
 
-  const isEmailValidated = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(data.email); // test from "data"   True False döndürür.
+  // get data from "this" when create;
+  // if process is updateOne, data will receive in "this._update"
+  const data = this?._update || this
+
+  // const emailRegExp = new RegExp("^\\w+([\\.-]?\\w+)*@\\w+([\\.-]?\\w+)*(\\.\\w{2,3})+$")
+  // const isEmailValidated = emailRegExp.test(data.email)
+  // const isEmailValidated = RegExp("^\\w+([\\.-]?\\w+)*@\\w+([\\.-]?\\w+)*(\\.\\w{2,3})+$").test(data.email)
+  const isEmailValidated = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(data.email) // test from "data".True False döndürür.
 
   if (isEmailValidated) {
-    const isPasswordValidated =/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&].{8,}$/.test(data.password);
-    
-    if (isPasswordValidated) {
-      
-        this.password = passwordEncrypt(this.password);
-      
-        next()                                          //---> bu kısmı MW olarak yazıp alamıyoruz mongoose schema'ya özel bir durum. bu şekilde kullanılmalı
-    } else {
-        throw new Error ( ' Password Rulez Broken ')
-    }
+
+      const isPasswordValidated = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&].{8,}$/.test(data.password)
+  
+      if (isPasswordValidated) {
+          
+          this.password = data.password = passwordEncrypt(data.password)
+
+          this._update = data // updateOne will wait data from "this._update".
+          next()                                        //---> bu kısmı MW olarak yazıp alamıyoruz mongoose schema'ya özel bir durum. bu şekilde kullanılmalı
+      } else {
+          
+          next( new Error('Password not validated.') )
+      }
   } else {
-        throw new Error ( ' Password Rulez Broken ')
-}
-});
+      
+      next( new Error('Email not validated.') )
+  }
+})
 /* ------------------------------------------------------- */
 module.exports = mongoose.model('User', UserSchema)
+
+
