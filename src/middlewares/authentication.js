ad7e239d920f99dcf59d3223359caeb3079928af
Ekrem <avekremyilmazturk@gmail.com>
2023-10-28 15:29:18 +0300

    up

--- src/middlewares/authentication.js
+++ src/middlewares/authentication.js
@@ -14,9 +14,9 @@ module.exports = async (req, res, next) => {
 
     const tokenKey = auth ? auth.split(' ') : null
 
-    if (tokenKey && tokenKey[0] == 'Token') {            //---> tokenKey var mı? ve birinci indeksi Token mı?
-        const tokenData = await Token.findOne({token: tokenKey[1]})
-        if (tokenData) req.user = await User.findOne({ _id: tokenData.userId })  //---> token varsa;  kullnaıcım = veritabanındaki _id token ile gelenle aynı olanı al,ata
+    if (tokenKey && tokenKey[0] == 'Token') {                                    //---> tokenKey var mı? ve sıfırıncı indeksi Token mı?
+        const tokenData = await Token.findOne({token: tokenKey[1]})              //---> Token'ı yakala , ayırdım ya 1.endekste tokenın kendisini al
+        if (tokenData) req.user = await User.findOne({ _id: tokenData.userId })  //---> token varsa;  kullanıcım = veritabanındaki _id token ile gelenle aynı olanı al,ata
         
     }
 
